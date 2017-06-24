1adc88620b96cdb52d5a33678779429c91d4b040
Alejandro Saucedo <axsauze@gmail.com>
2017-06-24 13:14:28 +0100

    info

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, Command, find_packages
-from distutils.command.install import install
+from setuptools.command.install import install
 import os
+import atexit
 
 currentFileDirectory = os.path.dirname(__file__)
 with open(os.path.join(currentFileDirectory, "README.md"), "r") as f:
@@ -9,6 +10,7 @@ with open(os.path.join(currentFileDirectory, "README.md"), "r") as f:
 # Maintain dependencies of requirements.txt
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+
 
 class CleanCommand(Command):
     """Custom clean command to tidy up the project root."""
@@ -20,12 +22,24 @@ class CleanCommand(Command):
     def run(self):
         os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info ./**/__pycache__ ./.eggs ./.cache')
 
-# Make sure nltk language packages are installed
+def post_install():
+    """
+    Install the nltk package after the wc.py package has been
+       successfully installed
+    """
+    import nltk
+    nltk.download('punkt')
+
 class FullInstall(install):
+    """
+    Create a class that handles the installation of our package
+       together with the post-installations that require the modules
+       to be installed
+    """
     def __init__(self, *args, **kwargs):
         super(FullInstall, self).__init__(*args, **kwargs)
-        import nltk
-        nltk.download('punkt')
+
+        atexit.register(post_install)
 
 print(find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]))
 
@@ -55,7 +69,7 @@ setup(
     test_suite='tests',
     cmdclass={
         'clean': CleanCommand,
-        'install_data': FullInstall
+        'install': FullInstall
     }
 )
 
